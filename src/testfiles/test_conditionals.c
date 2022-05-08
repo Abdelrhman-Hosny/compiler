4c74d22bd01d986110d3a151fbd49a2a55bf0f0c
Abdelrhman-Hosny <ahosny2792@gmail.com>
2022-05-08 23:08:28 +0200

    add booleans and conditions test

---
+++ src/testfiles/test_conditionals.c
@@ -0,0 +1,47 @@
+int main()
+{
+    if ( 1 )
+    {
+    }
+    else
+    {
+    }
+
+    if ( x == 5)
+    {
+    }
+    else if ( x == 6)
+    {
+    }
+    else if (x != 4)
+    {
+        if (x == 5)
+        {
+        }
+        else if (x == 6)
+        {
+        }
+    }
+    else
+    {
+    }
+    
+    switch (x)
+    {
+    case 2:
+        x = 3; 
+    case 5:
+        x = 4;
+    case 6:
+        break;
+    default:
+        break;
+    }
+
+    switch (x)
+    {
+        default:
+            break;
+    }
+
+}
