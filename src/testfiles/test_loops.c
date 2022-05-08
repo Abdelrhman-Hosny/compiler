d0772f90bdd5ae709087319249ca8d1c33c47705
Abdelrhman-Hosny <ahosny2792@gmail.com>
2022-05-08 21:42:53 +0200

    add test for loops

---
+++ src/testfiles/test_loops.c
@@ -0,0 +1,50 @@
+int main()
+{
+    for (int i = 0;;)
+    {
+
+    }
+
+    for (int i = 0; i < 10; i = i + 1)
+    {
+    }
+
+    for (i = 0; i < 10; i = i + 1)
+    {
+    }
+    {
+    }
+
+    for (;i < 10; i = i + 1)
+    {
+    }
+
+    for (;i < 10;)
+    {
+    }
+
+    for (;;)
+    {
+    }
+
+    do {
+
+    } while (i < 10);
+
+    do {
+
+    } while();
+
+    while (i > 2)
+    {
+    }
+
+    while()
+    {
+    }
+
+    while(0)
+    {
+    }
+
+}
