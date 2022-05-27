d613012135ad93c168165d77ebaf609e8ed0ceda
Abdelrhman-Hosny <ahosny2792@gmail.com>
2022-05-27 19:27:38 +0200

    add include gaurd and operation constants

--- src/constants.h
+++ src/constants.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #define INT_TYPE 0
 #define FLOAT_TYPE 1
 #define CHAR_TYPE 2
@@ -5,3 +7,9 @@
 #define VOID_TYPE 4
 
 #define IS_CONSTANT 1
+
+#define ADD_OP 0
+#define SUB_OP 1
+#define MUL_OP 2
+#define DIV_OP 3
+#define MOD_OP 4
