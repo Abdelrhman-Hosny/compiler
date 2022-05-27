e657a9b63422fa63e7c1fbd15a16c8e624526a3c
Abdelrhman-Hosny <ahosny2792@gmail.com>
2022-05-28 00:37:01 +0200

    add logical operators and unaryMinus

--- src/mathOpUtils.h
+++ src/mathOpUtils.h
@@ -24,11 +24,25 @@ struct ExpressionData * binaryMathExpression(struct ExpressionData * returnOp,
 
     returnOp = createExpressionMacro;
 
+    // handle Type of return
+    if (lType == FLOAT_TYPE || rType == FLOAT_TYPE)
+        returnOp->type = FLOAT_TYPE;
+    else
+        returnOp->type = INT_TYPE;
+   
+    // handle Validity of return
+    if (leftOp->valueIsValid == 0 || rightOp->valueIsValid == 0)
+        returnOp->valueIsValid = 0;
+    else
+        returnOp->valueIsValid = 1;
+
+    
+
+    // handle Value of return
     if (leftOp->valueIsValid == 1 && rightOp->valueIsValid == 1)
     { 
         if (lType == FLOAT_TYPE || rType == FLOAT_TYPE)
         {
-            returnOp->type = FLOAT_TYPE;
             switch (operation)
             {
             case ADD_OP:
@@ -60,7 +74,6 @@ struct ExpressionData * binaryMathExpression(struct ExpressionData * returnOp,
         }
         else
         {
-            returnOp->type = INT_TYPE;
             
             switch (operation)
             {
@@ -113,11 +126,7 @@ struct ExpressionData * binaryMathExpression(struct ExpressionData * returnOp,
             printf("Error: Mod operator can only be done on operands of type integer\n");
             return NULL;
         }  
-
-        returnOp->valueIsValid = 0;        
-    } else
-        returnOp->valueIsValid = 0;
-    
+    }
 
     free(leftOp);
     free(rightOp);
@@ -125,3 +134,133 @@ struct ExpressionData * binaryMathExpression(struct ExpressionData * returnOp,
     return returnOp;
 }
 
+
+struct ExpressionData * binaryLogicalExpression(struct ExpressionData * returnOp,
+                     struct ExpressionData * leftOp,
+                    struct ExpressionData * rightOp,
+                    int operation)
+{
+    int lType = leftOp->type;
+    int rType = rightOp->type;
+
+    if ( lType == CHAR_TYPE || rType == CHAR_TYPE)
+    {
+        printf("Error: Cannot perform logical operation on char\n");
+        return NULL;
+    }
+
+    returnOp = createExpressionMacro;
+
+    // return type is always int (1 or 0)
+    returnOp->type = INT_TYPE;
+
+    // handle validity
+    if (leftOp->valueIsValid == 1 && rightOp->valueIsValid == 1)
+        returnOp->valueIsValid = 1;
+    else
+        returnOp->valueIsValid = 0;
+
+    // handle value
+    if (leftOp->valueIsValid == 1 && rightOp->valueIsValid == 1)
+    {
+        switch (operation)
+        
+        {
+        case AND_OP:
+
+            returnOp->intValue = (getNumValue(leftOp) && getNumValue(rightOp));
+            break;
+        case OR_OP:
+
+            returnOp->intValue = (getNumValue(leftOp) || getNumValue(rightOp));
+            break;
+        case GEQ_OP:
+
+            returnOp->intValue = (getNumValue(leftOp) >= getNumValue(rightOp));
+            break;
+        case LEQ_OP:
+            
+            returnOp->intValue = (getNumValue(leftOp) <= getNumValue(rightOp));
+            break;
+        case EQ_OP:
+
+            returnOp->intValue = (getNumValue(leftOp) == getNumValue(rightOp));
+            break; 
+        
+        case LT_OP:
+
+            returnOp->intValue = (getNumValue(leftOp) < getNumValue(rightOp));
+            break;
+        
+        case GT_OP:
+
+            returnOp->intValue = (getNumValue(leftOp) > getNumValue(rightOp));
+            break;
+        default:
+            printf("Error: Invalid operation\n");
+            return NULL;
+            break;
+
+        }
+
+    }
+
+    free(leftOp);
+    free(rightOp);
+
+    return returnOp;
+}
+
+struct ExpressionData * unaryMinusMathExpression(struct ExpressionData * leftOp)
+{
+
+    if (leftOp->valueIsValid == 1)
+    {
+        if (leftOp->type == INT_TYPE)
+            leftOp->intValue = -leftOp->intValue;
+        else if (leftOp->type == FLOAT_TYPE)
+            leftOp->doubleValue = -leftOp->doubleValue;
+        else
+        {
+            printf("Error: Unary minus can only be applied to int or float\n");
+            return NULL;
+        }
+    }
+
+    // if it were valid it would still be valid
+    // if it were an int, it would be still be an int (same for float)
+    // so not required to handle any other errors.
+
+    return leftOp;
+
+}
+
+struct ExpressionData * notLogicalOperator(struct ExpressionData * leftOp)
+{
+    int lType = leftOp->type;
+
+    struct ExpressionData * returnOp = createExpressionMacro;
+
+    returnOp->type = INT_TYPE;
+    
+    if (leftOp->valueIsValid == 1)
+    {
+        if (lType == INT_TYPE)
+        {
+            returnOp->intValue = !leftOp->intValue;
+        }
+        else if (lType == FLOAT_TYPE)
+        {
+            returnOp->intValue = !leftOp->doubleValue;
+        }
+        else
+        {
+            printf("Error: Not operator can only be applied to int or float\n");
+            return NULL;
+        }
+    } 
+
+    free(leftOp);
+
+    return returnOp;
+}
